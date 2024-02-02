3917706044c5469d580acdf73eab6a2d36c35f07
arpu <a.putz@captic.io>
2024-02-02 15:45:55 +0100

    Revert "Skinning Shaders: Remove need skinning texture size uniform by using textureSize function (#27117)"
    This reverts commit 3e0f80faa748a52ec469c838053fe8a748eba27b.

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -1988,6 +1988,7 @@ class WebGLRenderer {
 						if ( skeleton.boneTexture === null ) skeleton.computeBoneTexture();
 
 						p_uniforms.setValue( _gl, 'boneTexture', skeleton.boneTexture, textures );
+						p_uniforms.setValue( _gl, 'boneTextureSize', skeleton.boneTextureSize );
 
 					} else {
 
